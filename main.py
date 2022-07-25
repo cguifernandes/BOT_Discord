7b33a11ce21ba8182342d50dd0a4de258874a423
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2022-07-25 20:53:46 -0300

    make it better

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import discord
-from decouple import config
 from discord.ext import commands
+import discord.ext
 import random
-import giphy_client
+import requests
+from decouple import config
 
 bot = commands.Bot(command_prefix = "<")
 bot.remove_command("help")
@@ -17,73 +18,69 @@ async def on_message(message):
     if message.author == bot.user:
         return
         
-
     if bot.user.mentioned_in(message):
         if "OLÁ".lower() in message.content.lower() or "OLA".lower() in message.content.lower():
             await message.channel.send(
                 f"Olá querido amigo, {name} :moyai: !"
             )
-        if len(message.content) >= 30:
+        if len(message.content) >= 40:
             await message.channel.send(
                 f"Ei, {name} não é para spammar :triumph: !"
             )
     await bot.process_commands(message)
 
 @bot.command(name="anime")
-async def anime(ctx, *, q):
-    APIKEY = config("APIKEY")
+async def anime(ctx, *, q="anime"):
+    TenorToken = "LIVDSRZULELA"
     listanimes = q.split(",")
     search = random.choice(listanimes)
-    api_instace = giphy_client.DefaultApi()
 
-    api_responce = api_instace.gifs_search_get(APIKEY, search, limit=5)
-    lst = list(api_responce.data)
-    giff = random.choice(lst)
+    if q == "anime":
+        await ctx.channel.send("Por favor, digite o que será sorteado.")
+    else:
+        response = requests.get("https://g.tenor.com/v1/search?q={}&key={}&limit=15".format(search, TenorToken))
+        data = response.json()
+        gif = random.choice(data["results"])
 
-    em = discord.Embed (
-        title = f"O anime escolhido foi: {search}",
-        color = ctx.author.color
-    )
-    em.set_image(url = f"https://media.giphy.com/media/{giff.id}/giphy.gif")
-    em.set_author(name=bot.user.name, icon_url=bot.user.avatar_url)
-    em.set_footer(text="Feito por " + bot.user.name)
-    await ctx.channel.send(embed=em) 
+        em = discord.Embed (
+            title = f"O anime sorteado foi: {search}",
+            color = ctx.author.color
+        )
 
-@bot.command(name="character")
-async def anime(ctx, *, q):
-    APIKEY = config("APIKEY")
-    listanimes = q.split(",")
-    search = random.choice(listanimes)
-    api_instace = giphy_client.DefaultApi()
-
-    api_responce = api_instace.gifs_search_get(APIKEY, search, limit=5)
-    lst = list(api_responce.data)
-    giff = random.choice(lst)
-
-    em = discord.Embed (
-        title = f"O anime escolhido foi: {search}",
-        color = ctx.author.color
-    )
-    em.set_image(url = f"https://media.giphy.com/media/{giff.id}/giphy.gif")
-    em.set_author(name=bot.user.name, icon_url=bot.user.avatar_url)
-    em.set_footer(text="Feito por " + bot.user.name)
-    await ctx.channel.send(embed=em)
+        em.set_image(url = gif['media'][0]['gif']['url'])
+        em.set_author(name = bot.user.name, icon_url = bot.user.avatar_url)
+        em.set_footer(text = "Feito por " + bot.user.name)
+        await ctx.channel.send(embed=em)
 
 @bot.command(name="ajuda")
-async def help(ctx):
-    url_image = "https://preview.redd.it/ngnenrma5i191.png?auto=webp&s=2d700f306dda3d85e5821d3be0da5558bec40728"    
+async def ajuda(ctx):
+    url_image = "https://leosigh.com/wp-content/uploads/2022/02/Miko-Iino-adorable-as-always-Season-3-promo-visual-520x374.jpg"    
 
     em = discord.Embed (
         title = "Ajuda :heart: ", 
-        description = 'Escrava no chat com "Olá" para que assim, o bot responda! \n oi', 
-        color = ctx.author.color, 
+        description = 'Olá à todos! Eu sou o Random, onde a minha principal funcionalidade é sortear animes (Está não é minha única função :laughing:) mas posso também sortear outras coisas da sua escolha! Estou aqui para a sua disposição.', 
+        color = ctx.author.color
     )
-    em.set_author(name=bot.user.name, icon_url=bot.user.avatar_url)
-    em.set_footer(text="Feito por " + bot.user.name)
-    em.set_thumbnail (url=url_image)
+    em.set_author(name = bot.user.name, icon_url = bot.user.avatar_url)
+    em.set_footer(text = "Feito por " + bot.user.name)
+    em.set_image (url = url_image)
+    em.add_field(name = "Propriedades", value = "- diga um olá para mim! Basta digitar olá e me mencionar.\n- anime: Sorteie quaisquer anime, sendo da sua escolha, e o resultado será mostrado com um GIF! :laughing: (A divisão do que será sorteado é através do uso da vírgula).\n- aleatorio: Sorteie qualquer coisa sendo da sua escolha, sem GIF :frowning2: (A divisão do que será sorteado é através do uso da vírgula).", inline = False)
+    await ctx.channel.send(embed=em) 
 
-    await ctx.send(embed=em)
+@bot.command(name="aleatorio")
+async def aleatorio(ctx, *, q="anime"):
+    listaleatorio = q.split(",")
+    search = random.choice(listaleatorio)
+
+    if q == "anime":
+        await ctx.channel.send("Por favor, digite o que será sorteado.")
+    else:
+        em = discord.Embed (
+            title = (f"O sorteado foi: {search} "),
+            color = ctx.author.color
+        )
+        em.set_author(name=bot.user.name, icon_url=bot.user.avatar_url)
+        await ctx.send(embed=em)
 
 TOKEN = config("TOKEN")
 bot.run(TOKEN)
-
