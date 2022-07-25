f7ed563be16db553e8f530a73820778b62c130d4
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2022-07-24 21:46:33 -0300

    bot finalizado

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import discord
 from decouple import config
 from discord.ext import commands
+import random
+import giphy_client
 
 bot = commands.Bot(command_prefix = "<")
 bot.remove_command("help")
 
-#event
-
 @bot.event
 async def on_ready():
     print(f"Estou pronto! {bot.user}")
-
-#Ola
 
 @bot.event
 async def on_message(message):
@@ -19,13 +17,56 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    if "OLÁ".lower() in message.content.lower() or "OLA".lower() in message.content.lower():
-        await message.channel.send(
-            f"Olá querido amigo, {name} :moyai: "
-        )
+    if bot.user.mentioned_in(message):
+        if "OLÁ".lower() in message.content.lower() or "OLA".lower() in message.content.lower():
+            await message.channel.send(
+                f"Olá querido amigo, {name} :moyai: !"
+            )
+        if len(message.content) >= 30:
+            await message.channel.send(
+                f"Ei, {name} não é para spammar :triumph: !"
+            )
     await bot.process_commands(message)
 
-#ajuda
+@bot.command(name="anime")
+async def anime(ctx, *, q):
+    APIKEY = config("APIKEY")
+    listanimes = q.split(",")
+    search = random.choice(listanimes)
+    api_instace = giphy_client.DefaultApi()
+
+    api_responce = api_instace.gifs_search_get(APIKEY, search, limit=5)
+    lst = list(api_responce.data)
+    giff = random.choice(lst)
+
+    em = discord.Embed (
+        title = f"O anime escolhido foi: {search}",
+        color = ctx.author.color
+    )
+    em.set_image(url = f"https://media.giphy.com/media/{giff.id}/giphy.gif")
+    em.set_author(name=bot.user.name, icon_url=bot.user.avatar_url)
+    em.set_footer(text="Feito por " + bot.user.name)
+    await ctx.channel.send(embed=em) 
+
+@bot.command(name="character")
+async def anime(ctx, *, q):
+    APIKEY = config("APIKEY")
+    listanimes = q.split(",")
+    search = random.choice(listanimes)
+    api_instace = giphy_client.DefaultApi()
+
+    api_responce = api_instace.gifs_search_get(APIKEY, search, limit=5)
+    lst = list(api_responce.data)
+    giff = random.choice(lst)
+
+    em = discord.Embed (
+        title = f"O anime escolhido foi: {search}",
+        color = ctx.author.color
+    )
+    em.set_image(url = f"https://media.giphy.com/media/{giff.id}/giphy.gif")
+    em.set_author(name=bot.user.name, icon_url=bot.user.avatar_url)
+    em.set_footer(text="Feito por " + bot.user.name)
+    await ctx.channel.send(embed=em)
 
 @bot.command(name="ajuda")
 async def help(ctx):
@@ -42,8 +83,6 @@ async def help(ctx):
 
     await ctx.send(embed=em)
 
-#run do bot
 TOKEN = config("TOKEN")
-
 bot.run(TOKEN)
 
